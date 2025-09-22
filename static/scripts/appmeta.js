d7e8dedfac1eefd40c278f0c8d55e1cf716a4452
Dmitry V. Luciv <dluciv@dluciv.name>
2025-09-22 12:11:22 +0300

    Луцив всегда со мной

---
+++ static/scripts/appmeta.js
@@ -0,0 +1,11 @@
+// Adding custom head fields is not supported by Duckquill theme,
+// so this is a kind of dirty hack to add a link.
+// TODO: log a feature request on this.
+(function () {
+  let linkTag = document.createElement("link");
+
+  linkTag.rel = "manifest";
+  linkTag.href = "/manifest.webmanifest";
+
+  document.head.appendChild(linkTag);
+})();
